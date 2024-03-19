2e9d1e1235f6241785e6dba24110e301de7f7d4c
Enrique Izel <eleaoizel@gmail.com>
2024-03-18 21:35:24 -0400

    feat: create GamesViewModel

---
+++ app/src/main/java/com/example/devgames/data/viewmodel/GamesViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.devgames.data.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.devgames.data.model.Game
+import com.example.devgames.data.model.GameResult
+import com.example.devgames.data.repository.GameRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class GamesViewModel(private val repository: GameRepository) : ViewModel() {
+
+  val gamesList = MutableLiveData<List<Game>>()
+  val errorMessage = MutableLiveData<String>()
+
+  fun getGames() {
+    val request = repository.getGames()
+    request.enqueue(object : Callback<GameResult> {
+      override fun onResponse(call: Call<GameResult>, response: Response<GameResult>) {
+        gamesList.postValue(response.body()?.results)
+      }
+
+      override fun onFailure(call: Call<GameResult>, t: Throwable) {
+        errorMessage.postValue(t.message)
+      }
+    })
+  }
+}
