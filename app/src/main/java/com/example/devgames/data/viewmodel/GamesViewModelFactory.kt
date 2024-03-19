7a80fc978e11ede01363b0829d468dd472f43c78
Enrique Izel <eleaoizel@gmail.com>
2024-03-18 21:35:08 -0400

    feat: create GamesViewModelFactory

---
+++ app/src/main/java/com/example/devgames/data/viewmodel/GamesViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.devgames.data.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.devgames.data.repository.GameRepository
+
+class GamesViewModelFactory(private val repository: GameRepository) : ViewModelProvider.Factory {
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    return if (modelClass.isAssignableFrom(GamesViewModel::class.java)) {
+      GamesViewModel(this.repository) as T
+    } else {
+      throw IllegalArgumentException("ViewModel Not Found")
+    }
+  }
+}
