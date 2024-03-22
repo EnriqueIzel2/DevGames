d12aa251835f4079988f598bb5e721fec624c3d5
Enrique Izel <eleaoizel@gmail.com>
2024-03-22 14:55:52 -0400

    feat: add click listener for items

--- app/src/main/java/com/example/devgames/ui/fragments/adapter/GamesAdapter.kt
+++ app/src/main/java/com/example/devgames/ui/fragments/adapter/GamesAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.devgames.ui.fragments.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,33 +7,37 @@ import com.bumptech.glide.Glide
 import com.example.devgames.data.model.Game
 import com.example.devgames.databinding.GameItemBinding
 
-class GamesAdapter(private val context: Context) :
+class GamesAdapter(private val gamesList: List<Game>, private val onItemClicked: (Game) -> Unit) :
   RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
-
-  private var gamesList = mutableListOf<Game>()
 
   inner class ViewHolder(private val binding: GameItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
+    private lateinit var game: Game
+
+    init {
+      binding.root.setOnClickListener {
+        if (::game.isInitialized) {
+          onItemClicked(game)
+        }
+      }
+    }
+
     fun bind(game: Game) {
+      this.game = game
       val title = binding.gameTitle
       val backgroundImage = binding.gameBackground
       val rating = binding.gameRating
 
       title.text = game.name
-      Glide.with(context).load(game.backgroundImage).into(backgroundImage)
+      Glide.with(binding.root.context).load(game.backgroundImage).into(backgroundImage)
       rating.text = game.rating.toString()
     }
   }
 
-  fun setGamesList(games: List<Game>) {
-    this.gamesList = games.toMutableList()
-    notifyDataSetChanged()
-  }
-
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
     val binding = GameItemBinding.inflate(
-      LayoutInflater.from(context),
+      LayoutInflater.from(parent.context),
       parent,
       false
     )
