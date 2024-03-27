7f69442932acd81109680fe86b0169e394de420f
Enrique Izel <eleaoizel@gmail.com>
2024-03-27 17:24:08 -0400

    WIP: add logs to catch the request

--- app/src/main/java/com/example/devgames/viewmodel/GamesViewModel.kt
+++ app/src/main/java/com/example/devgames/viewmodel/GamesViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.devgames.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.devgames.data.model.Game
@@ -9,6 +10,7 @@ import com.example.devgames.data.repository.GameRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.math.log
 
 class GamesViewModel(private val repository: GameRepository) : ViewModel() {
 
@@ -23,6 +25,7 @@ class GamesViewModel(private val repository: GameRepository) : ViewModel() {
       }
 
       override fun onFailure(call: Call<GameResult>, t: Throwable) {
+        Log.e("Details Debug", "onResponse: ${t.message}")
         errorMessage.postValue(t.message)
       }
     })
@@ -33,10 +36,12 @@ class GamesViewModel(private val repository: GameRepository) : ViewModel() {
     val request = repository.getGame(id)
     request.enqueue(object : Callback<GameDetails> {
       override fun onResponse(call: Call<GameDetails>, response: Response<GameDetails>) {
+        Log.i("Details Debug", "onResponse: body: ${response.body()}")
         gameDetails.postValue(response.body())
       }
 
       override fun onFailure(call: Call<GameDetails>, t: Throwable) {
+        Log.e("Details Debug", "onResponse: ${t.message}")
         errorMessage.postValue(t.message)
       }
     })
