855c52677d229e10da87a8b4f143f9172010f32b
Enrique Izel <eleaoizel@gmail.com>
2024-03-27 17:24:24 -0400

    WIP: add new attributes

--- app/src/main/java/com/example/devgames/data/model/GameDetails.kt
+++ app/src/main/java/com/example/devgames/data/model/GameDetails.kt
@@ -1,7 +1,16 @@
 package com.example.devgames.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class GameDetails(
   val id: Int,
   val name: String,
-  val description: String
+  val description: String,
+  val rating: Double,
+  @SerializedName("background_image")
+  val backgroundImage: String,
+  @SerializedName("background_image_additional")
+  val backgroundImageAdditional: String,
+//  val platforms: List<Platform>,
+//  val stores: List<String>,
 )
