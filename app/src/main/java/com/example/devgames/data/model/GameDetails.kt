99c966ee95a45392e8117b0afca3ffea64ad8419
Enrique Izel <eleaoizel@gmail.com>
2024-04-03 10:27:15 -0400

    feat: add new attributes

--- app/src/main/java/com/example/devgames/data/model/GameDetails.kt
+++ app/src/main/java/com/example/devgames/data/model/GameDetails.kt
@@ -11,7 +11,8 @@ data class GameDetails(
   val backgroundImage: String,
   @SerializedName("background_image_additional")
   val backgroundImageAdditional: String,
+  val website: String,
   val platforms: List<PlatformsResult>,
-  val stores: List<StoresResult>,
+  val stores: List<StoreResult>,
   val genres: List<Genre>
 )
