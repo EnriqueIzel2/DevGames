af003c3c75a446ebd9e51bd6defdbea2ec919f87
Enrique Izel <eleaoizel@gmail.com>
2024-04-05 10:25:58 -0400

    WIP: create mapper to fill recyclerViews for tags

--- app/src/main/java/com/example/devgames/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/devgames/ui/fragments/DetailsFragment.kt
@@ -10,9 +10,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.denzcoskun.imageslider.models.SlideModel
+import com.example.devgames.data.model.GameDetails
 import com.example.devgames.data.repository.GameRepository
 import com.example.devgames.databinding.FragmentDetailsBinding
 import com.example.devgames.service.RetrofitInitializer
+import com.example.devgames.ui.fragments.adapter.TagsItemAdapter
 import com.example.devgames.viewmodel.GamesViewModel
 import com.example.devgames.viewmodel.GamesViewModelFactory
 
@@ -20,11 +22,23 @@ class DetailsFragment : Fragment() {
 
   private lateinit var binding: FragmentDetailsBinding
   private val args: DetailsFragmentArgs by navArgs()
+
   private val gameImageSlider by lazy { binding.detailsImageSliderBanner }
   private val gameTitle by lazy { binding.detailsGameTitle }
+  private val gameRating by lazy { binding.detailsGameRating }
   private val gameWebsite by lazy { binding.detailsGameWebsite }
   private val gameDescription by lazy { binding.detailsGameDescription }
   private var imageList = mutableListOf(SlideModel(""), SlideModel(""))
+  private val recyclerViewGenres by lazy { binding.detailsGameListGenres }
+  private val recyclerViewPlatforms by lazy { binding.detailsGameListPlatforms }
+  private val recyclerViewStores by lazy { binding.detailsGameListStores }
+  private lateinit var mTagsItemAdapterGenres: TagsItemAdapter
+  private lateinit var mTagsItemAdapterPlatforms: TagsItemAdapter
+  private lateinit var mTagsItemAdapterStores: TagsItemAdapter
+
+  private lateinit var listGenresNames: List<String>
+  private lateinit var listPlatformsNames: List<String>
+  private lateinit var listStoresNames: List<String>
 
   private lateinit var gamesViewModel: GamesViewModel
   private val apiInitializer = RetrofitInitializer().createApiService()
@@ -49,7 +63,19 @@ class DetailsFragment : Fragment() {
 
     gamesViewModel.gameDetails.observe(requireActivity(), Observer { gameDetail ->
       gameTitle.text = gameDetail.name
+      gameRating.text = gameDetail.rating.toString()
       gameDescription.text = gameDetail.description
+
+      mapperTagName(gameDetail)
+
+      mTagsItemAdapterGenres = TagsItemAdapter(listGenresNames)
+      recyclerViewGenres.adapter = mTagsItemAdapterGenres
+
+      mTagsItemAdapterPlatforms = TagsItemAdapter(listPlatformsNames)
+      recyclerViewPlatforms.adapter = mTagsItemAdapterPlatforms
+
+      mTagsItemAdapterStores = TagsItemAdapter(listStoresNames)
+      recyclerViewStores.adapter = mTagsItemAdapterStores
 
       imageList[0] = SlideModel(gameDetail.backgroundImage)
       imageList[1] = SlideModel(gameDetail.backgroundImageAdditional)
@@ -59,6 +85,27 @@ class DetailsFragment : Fragment() {
     gamesViewModel.errorMessage.observe(this, Observer {
       Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
     })
+  }
+
+  private fun mapperTagName(gameDetails: GameDetails) {
+    val namesGenres = mutableListOf<String>()
+    val namesPlatforms = mutableListOf<String>()
+    val namesStores = mutableListOf<String>()
+
+    gameDetails.genres.map {
+      namesGenres.add(it.name)
+    }
+    listGenresNames = namesGenres
+
+    gameDetails.platforms.map {
+      namesPlatforms.add(it.platform.name)
+    }
+    listPlatformsNames = namesPlatforms
+
+    gameDetails.stores.map {
+      namesStores.add(it.store.name)
+    }
+    listStoresNames = namesStores
   }
 
   override fun onResume() {
